932473506cf16b2fb397c7a9dc22190da8af7c16
Lee seung chan <dltmdcks702@gmail.com>
2023-12-16 23:36:50 +0900

    feat: add router

--- src/api/routes/v1/server.route.js
+++ src/api/routes/v1/server.route.js
@@ -3,9 +3,11 @@ import serverController from '../../controllers/server.controller.js';
 
 const router = Router();
 
-router.get('/', () => {
-  return 'hello world'
-})
-router.get('/list', serverController.list);
+router.get('/', serverController.list.bind(serverController));
+router.post('/join', serverController.join.bind(serverController));
+router.post('/leave', serverController.leave.bind(serverController));
+router.post('/', serverController.create.bind(serverController));
+router.put('/', serverController.update.bind(serverController));
+router.delete('/', serverController.delete.bind(serverController));
 
 export default router;
