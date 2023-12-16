3e08eed18a8095e4216fdbe06f86ba1ef0f745db
Lee seung chan <dltmdcks702@gmail.com>
2023-12-16 23:29:10 +0900

    feat: add server controller for cud

--- src/api/controllers/server.controller.js
+++ src/api/controllers/server.controller.js
@@ -1,12 +1,19 @@
 import { StatusCodes } from 'http-status-codes';
 import prismaClient from '../../libs/prismaClient.js';
 import ServerDTO from '../dtos/server.dto.js';
+import serverService from '../services/server.service.js';
+import logger from '../../util/logger.js';
+import { authByToken } from '../../utils/functions.js';
 
 class ServerController {
   prismaClient;
+  serverService;
+  logger;
 
-  constructor({ prismaClient }) { 
+  constructor({ prismaClient, serverService, logger }) { 
     this.prismaClient = prismaClient;
+    this.serverService = serverService;
+    this.logger = logger;
   }
 
   async list(req,res,next) {
@@ -18,8 +25,47 @@ class ServerController {
       next(e);
     }
   }
+
+  async create(req, res, next) {
+    authByToken(req, res, next, (async (user) => {
+      try {
+        const server = await this.serverService.create({ user, server: req.body });
+
+        res.status(StatusCodes.CREATED).json(server);
+      } catch (error) {
+        this.logger.error(error);
+        next(error);
+      }
+    }).bind(this));
+  }
+
+  async update(req, res, next) {
+    authByToken(req, res, next, (async (user) => {
+      try {
+        const server = await this.serverService.update({ user, server: req.body });
+
+        res.status(StatusCodes.OK).json(server);
+      } catch (error) {
+        this.logger.error(error);
+        next(error);
+      }
+    }).bind(this));
+  }
+
+  async delete(req, res, next) {
+    authByToken(req, res, next, (async (user) => { 
+      try {
+        await this.serverService.delete({ user, server: req.body });
+
+        res.status(StatusCodes.NO_CONTENT).send();
+      } catch (error) { 
+        this.logger.error(error);
+        next(error);
+      }
+    }).bind(this));
+  }
 }
 
-const serverController = new ServerController({ prismaClient });
+const serverController = new ServerController({ prismaClient, serverService, logger });
 
 export default serverController;
