8833dfa20a0f9e1ae6879035dd34b09b6eae4a2d
Lee seung chan <dltmdcks702@gmail.com>
2023-12-25 13:12:55 +0900

    feat: add redis default setup

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,4 @@ export const PORT = process.env.PORT;
 export const LOG_ENV = process.env.LOG_ENV;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const ACCESS_TOKEN = 'accessToken';
+export const REDIS_PATH = process.env.REDIS_PATH;
